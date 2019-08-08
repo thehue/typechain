30aeccbf3a61c7585e670d664f0cbcd60a2aa782
wonderotter <gouk32@naver.com>
2019-08-08 15:04:08 +0900

    BlockChain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
-/*
-interface는 오류때문에 js로 컴파일 되지 않는다
-interface를 컴파일된 js에 보이고 싶을 때 class를 사용!
-*/
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-
-    constructor(name:string, age:number, gender?:string){ // ?는 중간에는 못쓴다 -> 맨마지막이나 끝부터 연속적으로만 가능
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
     }
 }
 
-const Nick = new Human('Nick',21,'male');
-
-const sayHi = (person:Human) :string => {
-    return `Hello, ${person.name}! Your age is ${person.age} and gender is ${person.gender}.`;
-}
-
-let result = sayHi(Nick);
-console.log(result);
+const genesisBlock: Block = new Block(0,"202020303020","","Hello",123456);
+let blockChain: [Block] = [genesisBlock];
+console.log(blockChain);
 
 export {};
