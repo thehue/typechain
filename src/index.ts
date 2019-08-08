65215c725a8cb36f2d19a222f0f5cee725e758a2
wonderotter <gouk32@naver.com>
2019-08-08 23:22:50 +0900

    Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJS from 'crypto-js';
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    //static method -> 클래스가 생성되지 않았어도 호출할 수 있는 함수
+    static calculateBlockHash = (index:number, previousHash:string, data:string, timestamp:number) :string => {
+        return CryptoJS.SHA256(index + previousHash + data + timestamp).toString();
+    }
+
     constructor(index: number,
         hash: string,
         previousHash: string,
@@ -18,7 +25,13 @@ class Block {
 }
 
 const genesisBlock: Block = new Block(0,"202020303020","","Hello",123456);
-let blockChain: [Block] = [genesisBlock];
-console.log(blockChain);
+let blockchain: Block[] = [genesisBlock];
+
+//블록체인이 얼마나 긴지 알려주는 함수 -> 블록체인에 블록을 하나 더 추가할 수 있기 때문
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
